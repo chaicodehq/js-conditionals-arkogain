476f3203a26b3bd8248e6ca09c41bcbdaf87f069
Arko Gain <37438306+arkogain@users.noreply.github.com>
2026-02-26 16:40:00 +0530

    updated-12

--- src/12-season-activity.js
+++ src/12-season-activity.js
@@ -40,14 +40,15 @@ export function getSeasonActivity(month, temperature) {
 
 	if (month === 12 || month === 1 || month === 2) {
 		season = "Winter";
-	} else if (month === 3 || month === 4 || month === 5) {
+	}
+	if (month === 3 || month === 4 || month === 5) {
 		season = "Spring";
-	} else if (month === 6 || month === 7 || month === 8) {
+	}
+	if (month === 6 || month === 7 || month === 8) {
 		season = "Summer";
-	} else if (month === 9 || month === 10 || month === 11) {
+	}
+	if (month === 9 || month === 10 || month === 11) {
 		season = "Autumn";
-	} else {
-		null;
 	}
 
 	let activity;
